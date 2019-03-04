5c1fb140c85935554178bb8d2555fcadca38de85
Fletcher Morris <odmoandtheboy@gmail.com>
2019-03-04 14:21:19 +0000

    Started work on renderer
    Core file now correctly initializes glew and glfw.
    
    Shader files now load properly when glew and glfw are initialized.

--- Crimsonite/Crimsonite/source/core.cpp
+++ Crimsonite/Crimsonite/source/core.cpp
@@ -2,14 +2,42 @@
 #include <stdio.h>
 #include <direct.h>
 
+#include <GL/glew.h>
+#include <glfw3.h>
+
 #include "asset/AssetManager.h"
 #include "ecs/ECS.h"
 
 namespace Crimson
 {
+	GLFWwindow * window;
 	void Test()
 	{
-		/*GLenum err = glewInit();
+		std::cout << "==========" << std::endl;
+		printf("CRIMSONITE\n");
+		std::cout << "==========" << std::endl;
+
+		std::cout << _getcwd(NULL, 0) << std::endl;
+
+
+		if (!glfwInit())
+		{
+			std::cout << "Failed to initialise GLFW!" << std::endl;
+			return;
+		}
+		std::cout << "Initialised GLFW (" << glfwGetVersionString() << ")" << std::endl;
+		glfwWindowHint(GLFW_SAMPLES, 4);
+		glEnable(GL_MULTISAMPLE);
+		window = glfwCreateWindow(1280, 720, "Crimsonite Engine", NULL, NULL);
+		if (!window)
+		{
+			std::cout << "Failed to create GLFW window!" << std::endl; return;
+		}
+		glfwMakeContextCurrent(window);
+
+
+
+		GLenum err = glewInit();
 		if (GLEW_OK != err)
 		{
 			std::cout << "Failed to initialise GLEW!" << std::endl;
@@ -18,16 +46,15 @@ namespace Crimson
 		{
 			std::cout << "FAILED TO INITIALISE GLEW MISSING GL VERSION" << std::endl;
 		}
-		std::cout << "Initialised GLEW (" << glewGetString(GLEW_VERSION) << ")" << std::endl;*/
+		std::cout << "Initialised GLEW (" << glewGetString(GLEW_VERSION) << ")" << std::endl;
+		std::cout << "Initialised OpenGL (" << glGetString(GL_VERSION) << ")" << std::endl;
 
-		printf("CRIMSONITE\n");
 
-		std::cout << _getcwd(NULL, 0) << std::endl;
 
-		AssetManager::Instance()->LoadMesh("cube", "G:/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/cube");
-		AssetManager::Instance()->WriteMeshFile(*AssetManager::Instance()->GetMesh("cube"), "G:/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/cube2.mesh");
+		AssetManager::Instance()->LoadMesh("cube", "C:/Users/fmorris/Documents/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/cube");
+		AssetManager::Instance()->WriteMeshFile(*AssetManager::Instance()->GetMesh("cube"), "C:/Users/fmorris/Documents/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/cube2.mesh");
 
-		//AssetManager::Instance()->LoadShader("shader", "G:/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/vertex.vert", "C:/Users/fmorris/Documents/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/fragment.frag");
+		AssetManager::Instance()->LoadShader("shader", "C:/Users/fmorris/Documents/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/vertex.vert", "C:/Users/fmorris/Documents/prco304-final-year-project-Fletcher-Morris/Demo (Output)/Debug/assets/fragment.frag");
 
 
 		EcsSystem ecs;

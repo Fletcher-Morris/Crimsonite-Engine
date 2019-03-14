b93d8fdfba2b73247c539a856ea714df7b950b5b
Fletcher <odmoandteboy@gmail.com>
2019-03-14 11:01:09 +0000

    Created 'Camera' component class

---
+++ Crimsonite/Crimsonite/source/ecs/components/Camera.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../ECS.h"
+
+
+class Camera : public EcsComponent
+{
+
+public:
+
+	virtual void OnInit() override;
+	virtual void OnUpdate() override;
+	virtual void OnFixedUpdate() override;
+	virtual void OnRender() override;
+	virtual void OnEnable() override;
+	virtual void OnDisable() override;
+
+	void SetCameraSettings(CameraSettings _newSettings);
+	void SetCameraSettings(float _fov);
+	void SetCameraSettings(float _near, float _far);
+	void SetCameraSettings(int _width, int _height)
+	CameraSettings GetCameraSettings() { return m_settings; }
+
+private:
+
+	CameraSettings m_settings;
+
+
+};
+
+struct CameraSettings
+{
+	int width = 265;
+	int height = 256;
+	float fov = 60.0f;
+	float nearClip = 0.1f;
+	float farClip = 100.0f;
+};

8b7150d80b9f28634eab070f0d5ba9aa276a121a
Fletcher Morris <odmoandtheboy@gmail.com>
2019-03-20 05:47:28 +0000

    Minor changes
    Added some missing files.

--- Crimsonite/Crimsonite/source/editor/Editor.h
+++ Crimsonite/Crimsonite/source/editor/Editor.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "../external/imgui/imgui.h"
+#include "../external/imgui/imgui_impl_glfw.h"
+#include "../external/imgui/imgui_impl_opengl3.h"
 
 class Editor
 {

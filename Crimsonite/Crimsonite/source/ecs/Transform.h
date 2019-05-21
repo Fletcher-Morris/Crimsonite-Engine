6cc57e2f5dbf45a65d71206cda84baf7eb93c61a
Fletcher Morris <odmoandtheboy@gmail.com>
2019-05-21 02:34:56 +0100

    Commented Transform header file

--- Crimsonite/Crimsonite/source/ecs/Transform.h
+++ Crimsonite/Crimsonite/source/ecs/Transform.h
@@ -20,66 +20,80 @@ public:
 		localPosition = glm::vec3();
 		localRotation = glm::vec3();
 		m_parentTransform = NULL;
-		scale = glm::vec3(1.0f,1.0f,1.0f);
+		scale = glm::vec3(1.0f, 1.0f, 1.0f);
 	}
 
-	//	The position of this transform in world-space.
+	//	The position of the transform in world-space.
 	glm::vec3 position;
-	//	The rotation of this transform.
+	//	The rotation of the transform.
 	glm::vec3 rotation;
-	//	The scale of this transform.
+	//	The scale of the transform.
 	glm::vec3 scale;
 
-	//	The local position of this transform in world-space.
+	//	The local position of the transform in world-space.
 	glm::vec3 localPosition;
-	//	The local rotation of this transform.
+	//	The local rotation of the transform.
 	glm::vec3 localRotation;
 
+	//	Return the forward vector3 from the Transform.
 	glm::vec3 Forward()
 	{
 		forward.x = sin(glm::radians(rotation.y + localRotation.y)) * cos(glm::radians(rotation.x + localRotation.x));
 		forward.y = sin(glm::radians(rotation.x + localRotation.x));
 		forward.z = cos(glm::radians(rotation.y + localRotation.y)) * cos(glm::radians(rotation.x + localRotation.x));
-forward = glm::normalize(forward);
-return forward;
+		forward = glm::normalize(forward);
+		return forward;
 	}
+	//	Return the backwards vector3 from the Transform.
 	glm::vec3 Back()
 	{
 		return -Forward();
 	}
+	//	Return the right vector3 from the Transform.
 	glm::vec3 Right()
 	{
 		right = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), Forward()));
 		return right;
 	}
+	//	Return the left vector3 from the Transform.
 	glm::vec3 Left()
 	{
 		return -Right();
 	}
+	//	Return the up vector3 from the Transform.
 	glm::vec3 Up()
 	{
 		up = glm::cross(Forward(), Right());
 		up = glm::normalize(up);
 		return up;
 	}
+	//	Return the down vector3 from the Transform.
 	glm::vec3 Down()
 	{
 		return -Up();
 	}
 
+	//	Set the Transform's local position.
 	void SetLocalPosition(glm::vec3 _newPos) { localPosition = _newPos; }
 	void SetLocalPosition(float _x, float _y, float _z) { localPosition = { _x,_y,_z }; }
+	//	Return the Transform's local position.
 	glm::vec3 GetLocalPosition() { InheritParentTransformValues(); return localPosition; }
 
+	//	Set the Transform's local rotation.
 	void SetLocalRotation(glm::vec3 _newRot) { localRotation = _newRot; }
 	void SetLocalRotation(float _x, float _y, float _z) { localRotation = { _x,_y,_z }; }
+	//	Return the Transform's local rotation.
 	glm::vec3 GetLocalRotation() { InheritParentTransformValues(); return localRotation; }
 
+	//	Set the Transform's scale.
 	void SetScale(glm::vec3 _newScale) { scale = _newScale; }
 	void SetScale(float _x, float _y, float _z) { scale = { _x,_y,_z }; }
+	//	Return the Transform's scale.
 	glm::vec3 GetScale() { return scale; }
 
+	//	Return the Transform's world position.
 	glm::vec3 GetWorldPosition() { InheritParentTransformValues(); return position; }
+	//	Return the Transform's world rotation.
 	glm::vec3 GetWorldRotation() { InheritParentTransformValues(); return rotation; }
 
 	//	Adjust the position with another Vec3.
@@ -126,14 +140,17 @@ return forward;
 		localRotation.z += _z;
 	}
 
+	//	Return the Transfom's current parent Transform.
 	Transform * GetParent()
 	{
 		return m_parentTransform;
 	}
+	//	Set the Transform's parent Transform, maintaining it's current world position & rotation.
 	void SetParent(Transform * _newParentTransform)
 	{
 		SetParent(_newParentTransform, true);
 	}
+	//	Set the Transform's parent Transform.
 	void SetParent(Transform * _newParentTransform, bool _maintainValues)
 	{
 		if (_newParentTransform == NULL)
@@ -157,10 +174,12 @@ return forward;
 			}
 		}
 	}
+	//	Make the transform independant from it's current parent, maintaining it's current world position & rotation.
 	void Unparent()
 	{
 		Unparent(true);
 	}
+	//	Make the transform independant from it's current parent.
 	void Unparent(bool _maintainValues)
 	{
 		if (m_parentTransform != NULL)
@@ -173,6 +192,7 @@ return forward;
 		m_parentTransform = NULL;
 	}
 
+	//	Inherit the Transform values from the parent.
 	void InheritParentTransformValues()
 	{
 		if (m_parentTransform == NULL)
@@ -187,6 +207,7 @@ return forward;
 		}
 	}
 
+	//	Serialize the Transform's properties to a string.
 	std::string Serialize() override
 	{
 		std::string serialized = "val ";
@@ -209,12 +230,14 @@ return forward;
 		serialized += std::to_string(GetScale().z);
 		return serialized;
 	}
+	//	Deserialise the Transform from the proveded string vector.
 	void Deserialize(std::vector<std::string> _data) override
 	{
 		SetLocalPosition(std::stof(_data[3]), std::stof(_data[4]), std::stof(_data[5]));
 		SetLocalRotation(std::stof(_data[6]), std::stof(_data[7]), std::stof(_data[8]));
 		SetScale(std::stof(_data[9]), std::stof(_data[10]), std::stof(_data[11]));
 	}
+	//	Draw the editor properties for the Transform.
 	void DrawEditorProperties()
 	{
 		ImGui::Text("Transform Properties");
@@ -243,10 +266,14 @@ return forward;
 
 private:
 
+	//	The saved forward vector from the transform's perspective.
 	glm::vec3 forward;
+	//	The saved right vector from the transform's perspective.
 	glm::vec3 right;
+	//	The saved up vector from the transform's perspective.
 	glm::vec3 up;
 
+	//	The Transform's parent Transform, if null, uses origin.
 	Transform * m_parentTransform;
 
 };
